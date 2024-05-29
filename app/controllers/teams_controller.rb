8a50cd65b620bdf98179e02b0f27ed95f9e3a123
CristianApostol2002291 <apostol.2002291@studenti.uniroma1.it>
2024-05-29 14:14:19 +0200

    removed useless routes

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -35,53 +35,6 @@ class TeamsController < ApplicationController
     @roster = @roster['rowSet'].map { |player| {id: player[14], name: player[3], nickname: player[4], num: player[6], position: player[7], height: player[8], weight: player[9], birth_date: player[10], age: player[11], exp: player[12], school: player[13], how_acquired: player[15]} }
   end
 
-  # GET /teams/new
-  def new
-    @team = Team.new
-  end
-
-  # GET /teams/1/edit
-  def edit
-  end
-
-  # POST /teams or /teams.json
-  def create
-    @team = Team.new(team_params)
-
-    respond_to do |format|
-      if @team.save
-        format.html { redirect_to team_url(@team), notice: "Team was successfully created." }
-        format.json { render :show, status: :created, location: @team }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @team.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /teams/1 or /teams/1.json
-  def update
-    respond_to do |format|
-      if @team.update(team_params)
-        format.html { redirect_to team_url(@team), notice: "Team was successfully updated." }
-        format.json { render :show, status: :ok, location: @team }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @team.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /teams/1 or /teams/1.json
-  def destroy
-    @team.destroy
-
-    respond_to do |format|
-      format.html { redirect_to teams_url, notice: "Team was successfully destroyed." }
-      format.json { head :no_content }
-    end
-  end
-
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_team
