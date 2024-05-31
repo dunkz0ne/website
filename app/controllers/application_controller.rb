1ea936e62ca4c31e99380722e556ffa7e42146e3
Pietro Costanzi Fantini <pietrocostanzi39@gmail.com>
2024-05-31 16:49:12 +0200

    completata la sezione players

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,2 @@
 class ApplicationController < ActionController::Base
-    before_action :set_team
-
-    private
-
-    def set_team
-        @user = User.find(session[:user_id]) if session[:user_id]
-        @team = Team.where(id: @user.team_id).first if @user
-    end
 end
