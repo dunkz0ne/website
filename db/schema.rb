bde14e15e975236bd847b8c0833363b2b44576b8
CristianApostol2002291 <apostol.2002291@studenti.uniroma1.it>
2024-07-30 11:02:30 +0200

    Bug fix

--- db/schema.rb
+++ db/schema.rb
@@ -89,8 +89,8 @@ ActiveRecord::Schema.define(version: 2024_07_29_142101) do
     t.string "provider"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "bio"
     t.string "type"
+    t.string "bio"
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
