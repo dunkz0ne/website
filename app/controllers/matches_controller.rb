8adb56ddd0e2b383b89ef233e5a21b92026742f8
Pietro Costanzi Fantini <pietrocostanzi39@gmail.com>
2024-09-10 13:05:48 +0200

    Update della pagina matches

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -2,7 +2,7 @@ class MatchesController < ApplicationController
     def index
       @user = User.find(session[:user_id]) if session[:user_id]
       @team = Team.find(@user.team_id) if @user
-  
+      
       # PLAYOFFS
   
       url = URI.parse("http://localhost:5001/api/teams/#{@team.api}/schedule/playoffs")
@@ -11,7 +11,7 @@ class MatchesController < ApplicationController
       playoffs_data_raw = JSON.parse(response.body) # ["Team_ID", "Game_ID", "GAME_DATE", "MATCHUP", "WL", "W", "L", "W_PCT", "MIN", "FGM", "FGA", "FG_PCT", "FG3M", "FG3A", "FG3_PCT", "FTM", "FTA", "FT_PCT", "OREB", "DREB", "REB", "AST", "STL", "BLK", "TOV", "PF", "PTS"]
       playoffs_data = playoffs_data_raw['resultSets'][0]['rowSet'].map { |game| { team_id: game[0], game_id: game[1], game_date: game[2], matchup: game[3], wl: game[4], w: game[5], l: game[6], w_pct: game[7], min: game[8], fgm: game[9], fga: game[10], fg_pct: game[11], fg3m: game[12], fg3a: game[13], fg3_pct: game[14], ftm: game[15], fta: game[16], ft_pct: game[17], oreb: game[18], dreb: game[19], reb: game[20], ast: game[21], stl: game[22], blk: game[23], tov: game[24], pf: game[25], pts: game[26] }}
   
-      @matches_schedule = []
+      @matches_schedule_playoffs = []
   
       playoffs_data.each do |game|
           if game[:matchup].include?('@')
@@ -24,12 +24,14 @@ class MatchesController < ApplicationController
               away_team = team_list[1]
           end
   
-          @matches_schedule.push({ home_team: home_team, away_team: away_team, game_date: game[:game_date], game_id: game[:game_id], wl: game[:wl], home_points: nil, away_points: nil })
+          @matches_schedule_playoffs.push({ home_team: home_team, away_team: away_team, game_date: game[:game_date], game_id: game[:game_id], wl: game[:wl], home_points: nil, away_points: nil })
       end
   
-      @n_playoffs = @matches_schedule.length
+      @n_playoffs = @matches_schedule_playoffs.length
   
       # REGULAR SEASON
+
+      @matches_schedule_regular = []
   
       url = URI.parse("http://localhost:5001/api/teams/#{@team.api}/schedule/regular")
       response = Net::HTTP.get_response(url)
@@ -50,13 +52,13 @@ class MatchesController < ApplicationController
               away_team = team_list[1]
           end
   
-          @matches_schedule.push({ home_team: home_team, away_team: away_team, game_date: game[:game_date], game_id: game[:game_id], wl: game[:wl], home_points: nil, away_points: nil })
+          @matches_schedule_regular.push({ home_team: home_team, away_team: away_team, game_date: game[:game_date], game_id: game[:game_id], wl: game[:wl], home_points: nil, away_points: nil })
       end
   
       @playoffs_schedule = []
       @regular_schedule = []
   
-      @matches_schedule.each_with_index do |game, index|
+      @matches_schedule_playoffs.each_with_index do |game, index|
           if index > params[:number].to_i - 1
               break
           end
@@ -70,11 +72,24 @@ class MatchesController < ApplicationController
           game[:home_points] = match_data['resultSets'][5]['rowSet'][0][22]
           game[:away_points] = match_data['resultSets'][5]['rowSet'][1][22]
   
-          if index < @n_playoffs
-              @playoffs_schedule.push(game)
-          else
-              @regular_schedule.push(game)
+            @playoffs_schedule.push(game)
+      end
+
+      @matches_schedule_regular.each_with_index do |game, index|
+          if index > params[:number].to_i - 1
+              break
           end
+  
+          url = URI.parse("http://localhost:5001/api/matches/#{game[:game_id]}")
+          response = Net::HTTP.get_response(url)
+  
+          match_data = JSON.parse(response.body)
+          game[:home_team] = Team.where(api: match_data['resultSets'][0]['rowSet'][0][6]).first
+          game[:away_team] = Team.where(api: match_data['resultSets'][0]['rowSet'][0][7]).first
+          game[:home_points] = match_data['resultSets'][5]['rowSet'][0][22]
+          game[:away_points] = match_data['resultSets'][5]['rowSet'][1][22]
+  
+            @regular_schedule.push(game)
       end
   
     end
