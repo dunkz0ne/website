2e5f1b35471f2741ff8bf4288f44d32076e1c74c
Federico Gerardi <38018471+iGieri@users.noreply.github.com>
2024-09-12 12:04:19 +0200

    all test now are working

--- features/step_definitions/user_steps.rb
+++ features/step_definitions/user_steps.rb
@@ -1,5 +1,6 @@
 Given("I am on the home page") do
   visit root_path
+  click_button('Join Us')
 end
 
 When("I click on Sign In with Facebook") do
@@ -7,7 +8,7 @@ When("I click on Sign In with Facebook") do
 end
 
 When("I submit the registration form") do
-  expect(page).to have_current_path(new_user_path)
+  expect(page).to have_current_path(complete_registration_path)
 
   @team = Team.first
 
@@ -15,8 +16,8 @@ When("I submit the registration form") do
   fill_in 'user_bio', with: 'I am a test'
   attach_file('user_photo', Rails.root.join('features', 'files', 'test_image.png'))
 
-  expect(page).to have_button('Create User')
-  click_button('Create User')
+  expect(page).to have_button('Complete Registration')
+  click_button('Complete Registration')
 end
 
 Then("I should be on the dashboard page") do
@@ -35,10 +36,12 @@ Given("I am logged in with Facebook") do
   })
 
   # Visit the sign in path
-  visit auth_facebook_callback_path
+  visit user_facebook_omniauth_callback_path
 
-  if page.has_button?('Create User')
-    click_button('Create User')
+  select(Team.first.name, from: 'user_team_id')
+
+  if page.has_button?('Complete Registration')
+    click_button('Complete Registration')
   end
 
   visit user_dashboard_path
