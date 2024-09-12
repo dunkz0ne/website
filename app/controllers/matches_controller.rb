f1732175cdd8a542f81c6eda79c16a0620a0fde7
Pietro Costanzi Fantini <pietrocostanzi39@gmail.com>
2024-09-12 03:02:34 +0200

    Update dettagli matches

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -2,7 +2,7 @@ class MatchesController < ApplicationController
     def index
       @user = User.find(session[:user_id]) if session[:user_id]
       @team = Team.find(@user.team_id) if @user
-      
+
       # PLAYOFFS
   
       url = URI.parse("http://localhost:5001/api/teams/#{@team.api}/schedule/playoffs")
@@ -95,6 +95,9 @@ class MatchesController < ApplicationController
     end
   
     def show
+      @user = User.find(session[:user_id]) if session[:user_id]
+      @team = Team.find(@user.team_id) if @user
+
       @match = params[:id]
   
       url = URI.parse("http://localhost:5001/api/matches/#{@match}")
@@ -102,21 +105,49 @@ class MatchesController < ApplicationController
   
       match_data = JSON.parse(response.body)
   
-      match_summary = match_data['resultSets'][0]['rowSet'][0]
-      @match_summary = {game_date: match_summary[0], game_sequence: match_summary[1], game_id: match_summary[2], game_status_id: match_summary[3], game_status_text: match_summary[4], gamecode: match_summary[5], home_team_id: match_summary[6], visitor_team_id: match_summary[7], season: match_summary[8], live_period: match_summary[9], broadcaster: match_summary[11]}
-  
-      match_officials = match_data['resultSets'][2]['rowSet'][0]
+      match_summary = match_data['resultSets'][0]['rowSet'][0] # ["GAME_DATE_EST", "GAME_SEQUENCE", "GAME_ID", "GAME_STATUS_ID", "GAME_STATUS_TEXT", "GAMECODE", "HOME_TEAM_ID", "VISITOR_TEAM_ID", "SEASON", "LIVE_PERIOD", "LIVE_PC_TIME", "NATL_TV_BROADCASTER_ABBREVIATION","LIVE_PERIOD_TIME_BCAST","WH_STATUS"]
+      @match_summary = {game_date_est: match_summary[0], game_sequence: match_summary[1], game_id: match_summary[2], game_status_id: match_summary[3], game_status_text: match_summary[4], gamecode: match_summary[5], home_team_id: match_summary[6], visitor_team_id: match_summary[7], season: match_summary[8], live_period: match_summary[9], live_pc_time: match_summary[10], natl_tv_broadcaster_abbreviation: match_summary[11], live_period_time_bcast: match_summary[12], wh_status: match_summary[13]}
+      
+      match_officials = match_data['resultSets'][2]['rowSet']
       @match_officials = match_officials.map { |official| {first_name: official[1], last_name: official[2], jersey_number: official[3]} }
+
+      inactive_players = match_data['resultSets'][3]['rowSet'] # ["PLAYER_ID","FIRST_NAME","LAST_NAME","JERSEY_NUM","TEAM_ID","TEAM_CITY","TEAM_NAME","TEAM_ABBREVIATION"],
+      @inactive_players = inactive_players.map { |player| {id: player[0], first_name: player[1], last_name: player[2], jersey_number: player[3], team_id: player[4], team_city: player[5], team_name: player[6], team_abbreviation: player[7]} }
+
+      match_info = match_data['resultSets'][4]['rowSet'][0] # ["GAME_DATE", "ATTENDANCE", "GAME_TIME"]
+      @match_info = {game_date: match_info[0], attendance: match_info[1], game_time: match_info[2]}
   
-      match_linescore = match_data['resultSets'][4]['rowSet'][0] # ["GAME_DATE_EST", "GAME_SEQUENCE", "GAME_ID", "TEAM_ID", "TEAM_ABBREVIATION", "TEAM_CITY_NAME", "TEAM_NICKNAME", "TEAM_WINS_LOSSES", "PTS_QTR1", "PTS_QTR2", "PTS_QTR3", "PTS_QTR4", "PTS_OT1", "PTS_OT2", "PTS_OT3", "PTS_OT4", "PTS_OT5", "PTS_OT6", "PTS_OT7", "PTS_OT8", "PTS_OT9", "PTS_OT10", "PTS"]
-      match_linescore = match_linescore.map { |team| {team_id: team[3], team_abbreviation: team[4], team_city_name: team[5], team_nickname: team[6], team_wins_losses: team[7], pts_qtr1: team[8], pts_qtr2: team[9], pts_qtr3: team[10], pts_qtr4: team[11], pts_ot1: team[12], pts_ot2: team[13], pts_ot3: team[14], pts_ot4: team[15], pts_ot5: team[16], pts_ot6: team[17], pts_ot7: team[18], pts_ot8: team[19], pts_ot9: team[20], pts_ot10: team[21], pts: team[22]} }
+      match_linescore = match_data['resultSets'][5]['rowSet'] # ["GAME_DATE_EST", "GAME_SEQUENCE", "GAME_ID", "TEAM_ID", "TEAM_ABBREVIATION", "TEAM_CITY_NAME", "TEAM_NICKNAME", "TEAM_WINS_LOSSES", "PTS_QTR1", "PTS_QTR2", "PTS_QTR3", "PTS_QTR4", "PTS_OT1", "PTS_OT2", "PTS_OT3", "PTS_OT4", "PTS_OT5", "PTS_OT6", "PTS_OT7", "PTS_OT8", "PTS_OT9", "PTS_OT10", "PTS"]
+      match_linescore = match_linescore.map { |team| {game_date_est: DateTime.parse(team[0]), team_id: team[3], team_abbreviation: team[4], team_city_name: team[5], team_nickname: team[6], team_wins_losses: team[7], pts_qtr1: team[8], pts_qtr2: team[9], pts_qtr3: team[10], pts_qtr4: team[11], pts_ot1: team[12], pts_ot2: team[13], pts_ot3: team[14], pts_ot4: team[15], pts_ot5: team[16], pts_ot6: team[17], pts_ot7: team[18], pts_ot8: team[19], pts_ot9: team[20], pts_ot10: team[21], pts: team[22]} }
   
       @match_linescore_home = match_linescore[0]
-      @match_linescore_away = match_linescore[1]
+      @match_linescore_away = match_linescore[1]  
   
+      match_last_meeting = match_data['resultSets'][6]['rowSet'][0] # ["GAME_ID", "LAST_GAME_ID", "LAST_GAME_DATE_EST", "LAST_GAME_HOME_TEAM_ID", "LAST_GAME_HOME_TEAM_CITY", "LAST_GAME_HOME_TEAM_NAME", "LAST_GAME_HOME_TEAM_ABBREVIATION", "LAST_GAME_HOME_TEAM_POINTS", "LAST_GAME_VISITOR_TEAM_ID", "LAST_GAME_VISITOR_TEAM_CITY", "LAST_GAME_VISITOR_TEAM_NAME", "LAST_GAME_VISITOR_TEAM_CITY1", "LAST_GAME_VISITOR_TEAM_POINTS"]
+      @match_last_meeting = {game_id: match_last_meeting[0], last_game_id: match_last_meeting[1], last_game_date_est: DateTime.parse(match_last_meeting[2]), last_game_home_team_id: match_last_meeting[3], last_game_home_team_city: match_last_meeting[4], last_game_home_team_name: match_last_meeting[5], last_game_home_team_abbreviation: match_last_meeting[6], last_game_home_team_points: match_last_meeting[7], last_game_visitor_team_id: match_last_meeting[8], last_game_visitor_team_city: match_last_meeting[9], last_game_visitor_team_name: match_last_meeting[10], last_game_visitor_team_city1: match_last_meeting[11], last_game_visitor_team_points: match_last_meeting[12]}
   
-      match_last_meeting = match_data['resultSets'][5]['rowSet'][0] # ["GAME_ID", "LAST_GAME_ID", "LAST_GAME_DATE_EST", "LAST_GAME_HOME_TEAM_ID", "LAST_GAME_HOME_TEAM_CITY", "LAST_GAME_HOME_TEAM_NAME", "LAST_GAME_HOME_TEAM_ABBREVIATION", "LAST_GAME_HOME_TEAM_POINTS", "LAST_GAME_VISITOR_TEAM_ID", "LAST_GAME_VISITOR_TEAM_CITY", "LAST_GAME_VISITOR_TEAM_NAME", "LAST_GAME_VISITOR_TEAM_CITY1", "LAST_GAME_VISITOR_TEAM_POINTS"]
-      @match_last_meeting = {game_id: match_last_meeting[0], last_game_id: match_last_meeting[1], last_game_date_est: match_last_meeting[2], last_game_home_team_id: match_last_meeting[3], last_game_home_team_city: match_last_meeting[4], last_game_home_team_name: match_last_meeting[5], last_game_home_team_abbreviation: match_last_meeting[6], last_game_home_team_points: match_last_meeting[7], last_game_visitor_team_id: match_last_meeting[8], last_game_visitor_team_city: match_last_meeting[9], last_game_visitor_team_name: match_last_meeting[10], last_game_visitor_team_city1: match_last_meeting[11], last_game_visitor_team_points: match_last_meeting[12]}
+      @last_meeting_home_team = Team.find_by(api: @match_last_meeting[:last_game_home_team_id])
+      @last_meeting_away_team = Team.find_by(api: @match_last_meeting[:last_game_visitor_team_id])
+
+      @away_team = Team.find_by(api: @match_summary[:visitor_team_id])
+      @home_team = Team.find_by(api: @match_summary[:home_team_id])
+
+      url = URI.parse("http://localhost:5001/api/teams/#{@match_summary[:home_team_id].to_s}/roster")
+      response = Net::HTTP.get_response(url)
   
+      home_team_data = JSON.parse(response.body)
+  
+      @home_team_players = home_team_data['resultSets'][0] # ["TeamID", "SEASON", "LeagueID", "PLAYER", "NICKNAME", "PLAYER_SLUG", "NUM", "POSITION", "HEIGHT", "WEIGHT", "BIRTH_DATE", "AGE", "EXP", "SCHOOL", "PLAYER_ID", "HOW_ACQUIRED"],
+      @home_team_players = @home_team_players['rowSet'].map { |player| {id: player[14], name: player[3], nickname: player[4], num: player[6], position: player[7] } }  
+
+      url = URI.parse("http://localhost:5001/api/teams/#{@match_summary[:visitor_team_id].to_s}/roster")
+      response = Net::HTTP.get_response(url)
+  
+      away_team_data = JSON.parse(response.body)
+  
+      @away_team_players = away_team_data['resultSets'][0] # ["TeamID", "SEASON", "LeagueID", "PLAYER", "NICKNAME", "PLAYER_SLUG", "NUM", "POSITION", "HEIGHT", "WEIGHT", "BIRTH_DATE", "AGE", "EXP", "SCHOOL", "PLAYER_ID", "HOW_ACQUIRED"],
+      @away_team_players = @away_team_players['rowSet'].map { |player| {id: player[14], name: player[3], nickname: player[4], num: player[6], position: player[7] } }  
+
+
     end
   end
